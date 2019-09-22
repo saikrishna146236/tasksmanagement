2682fbfe87605670a130d8fcc7ba17dcafc09917
g-mothy <gobillamothy85@gmail.com>
2019-09-23 01:07:34 +0530

    phase_1

---
+++ backendservice/pasttasks.js
@@ -0,0 +1,25 @@
+const mongoDbService = require('../backendservice/mongodbservice')
+const bodyParser = require('body-parser')
+const authHandler = require('./authhandler'); 
+const commonService=require('../backendservice/common');
+
+exports.getAllpastTaskManagementOperation = (app) => {
+    
+
+    app.get('/retrievepast', bodyParser.json(), (request, response) => {
+        var cutoff = new Date();
+        mongoDbService.assets.find({endDate:{$lt:cutoff}})
+            
+            .sort({endDate:1})
+            .limit(10)
+            .then(val => {
+                console.log(val);
+                response.json(val);
+
+            }).catch(err => {
+                console.error("retrieveretrieve operation in errr"+err);
+            })
+
+    });
+    
+}
